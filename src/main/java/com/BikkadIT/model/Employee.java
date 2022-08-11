4a913178964fcde79965162db2b30b154df9c0c3
jadhavgajanan <gajananjadhav1020@gmail.com>
2022-08-11 15:58:04 +0530

    SBUHWR-5 CREATE THE MODEL CLASS

--- src/main/java/com/BikkadIT/model/Employee.java
+++ src/main/java/com/BikkadIT/model/Employee.java
@@ -1,5 +1,46 @@
 package com.BikkadIT.model;
 
-public class Employee {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private int empId;
+	private String empName;
+	
+	private double empSalary;
+	private String  empPwd;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(double empSalary) {
+		this.empSalary = empSalary;
+	}
+	public String getEmpPwd() {
+		return empPwd;
+	}
+	public void setEmpPwd(String empPwd) {
+		this.empPwd = empPwd;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + ", empPwd=" + empPwd
+				+ "]";
+	}
 }
